d11302ac730accb01c2ef590d8a8dded0fcf9db0
Vlad <vladislav.barzykin@gmail.com>
2022-06-07 22:03:47 +0300

    Initial commit

---
+++ src/main/java/org/example/Start.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Start extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello World!");
+    }
+}
